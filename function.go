a5ca9ed67c92b006d3789c7061147c934b68bad6
Vitaliy Zhhuta <zhhuta@gmail.com>
2020-10-27 07:55:42 +0200

    first commit

---
+++ function.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-ping/ping"
+)
+
+//Ping test conneciton
+func Ping() {
+	pinger, err := ping.NewPinger("www.google.com")
+	if err != nil {
+		panic(err)
+	}
+	pinger.Count = 3
+	pinger.Run()                 // blocks until finished
+	stats := pinger.Statistics() // get send/receive/rtt stats
+
+	fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
+	fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
+		stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
+	fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
+		stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
+
+	fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
+
+}
+
+func main() {
+	Ping()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zhhuta/gcf-ping
+
+go 1.15
+
+require github.com/go-ping/ping v0.0.0-20201022122018-3977ed72668a
